46edd2f62fd6b1e787c5b2f612622c8147f69697
俞育襦 <sherry2000307@gmail.com>
2022-09-17 15:09:04 +0800

    feat:add db tag

--- mpg_return.go
+++ mpg_return.go
@@ -22,119 +22,119 @@ type MPGGatewayResult struct {
 
 type TradeInfo struct {
 	//回傳狀態
-	Status string
+	Status string `db:"status"`
 	//回傳訊息
-	Message string
+	Message string `db:"message"`
 	//回傳參數
 	Result Result
 }
 type Result struct {
 	// **所有支付方式共同回傳參數**
 	//商店代號
-	MerchantID string
+	MerchantID string `db:"merchant_id"`
 	//交易金額
-	Amt int
+	Amt int `db:"amt"`
 	//藍新金流交易序號
-	TradeNo string
+	TradeNo string `db:"trade_no"`
 	//商店訂單編號
-	MerchantOrderNo string
+	MerchantOrderNo string `db:"merchant_order_no"`
 	//支付方式
-	PaymentType string
+	PaymentType string `db:"payment_type"`
 	//回傳格式
-	RespondType string
+	RespondType string `db:"response_type"`
 	//支付完成時間
-	PayTime string
+	PayTime string `db:"pay_time"`
 	//交易 IP
-	IP string
+	IP string `db:"ip"`
 	//款項保管銀行
-	EscrowBank string
+	EscrowBank string `db:"escrow_bank"`
 	//**信用卡支付回傳參數（一次付清、Google Pay、Samaung Pay、國民旅遊卡、銀聯）**
 	//收單金融機構
-	AuthBank string
+	AuthBank string `db:"auth_bank"`
 	//金融機構回應碼
-	RespondCode string
+	RespondCode string `db:"response_code"`
 	//授權碼
-	Auth string
+	Auth string `db:"auth"`
 	//卡號前六碼
-	Card6No string
+	Card6No string `db:"card_six_no"`
 	//卡號末四碼
-	Card4No string
+	Card4No string `db:"card_four_no"`
 	//分期-期別
-	Inst int
+	Inst int `db:"inst"`
 	//分期-首期金額
-	InstFirst int
+	InstFirst int `db:"inst_first"`
 	//分期-每期金額
-	InstEach int
+	InstEach int `db:"inst_each"`
 	//ECI
 	//1.3D 回傳值 eci=1,2,5,6，代表為 3D 交易。
 	//2.若交易送至收單機構授權時已是失敗狀態，則本欄位的值會以空值回傳。
-	ECI string
+	ECI string `db:"eci"`
 	//信用卡快速結帳使用狀態
-	TokenUseStatus string
+	TokenUseStatus string `db:"token_use_status"`
 	//紅利折抵後實際金額
-	RedAmt string
+	RedAmt string `db:"red_amt"`
 	//交易類別
-	PaymentMethod string
+	PaymentMethod string `db:"payment_method"`
 	//外幣金額
-	DCC_Amt int
+	DCC_Amt int `db:"dcc_amt"`
 	//匯率
-	DCC_Rate int
+	DCC_Rate int `db:"dcc_rate"`
 	//風險匯率
-	DCC_Markup int
+	DCC_Markup int `db:"dcc_markup"`
 	//幣別
-	DCC_Currency string
+	DCC_Currency string `db:"dcc_currency"`
 	//幣別代碼
-	DCC_Currency_Code string
+	DCC_Currency_Code string `db:"dcc_currency_code"`
 	//**WEBATM、ATM 繳費回傳參數**
 	//付款人金融機構代碼
-	PayBankCode string
+	PayBankCode string `db:"pay_bank_code"`
 	//付款人金融機構帳號末五碼
-	PayerAccount5Code string
+	PayerAccount5Code string `db:"payer_account_five_code"`
 	//**超商代碼繳費回傳參數**
 	//繳費代碼
-	CodeNo string
+	CodeNo string `db:"code_no"`
 	//繳費門市類別 超商類別名稱
-	StoreType string
+	StoreType string `db:"store_type"`
 	//繳費門市代號
-	StoreID string
+	StoreID string `db:"store_id"`
 	//**超商條碼繳費回傳參數**
 	//第一段條碼
-	Barcode_1 string
+	Barcode_1 string `db:"barcode_1"`
 	//第二段條碼
-	Barcode_2 string
+	Barcode_2 string `db:"barcode_2"`
 	//第三段條碼
-	Barcode_3 string
+	Barcode_3 string `db:"barcode_3"`
 	//付款次數
-	RepayTimes string
+	RepayTimes string `db:"repay_times"`
 	//繳費超商
-	PayStore string
+	PayStore string `db:"pay_store"`
 	//**超商物流回傳參數**
 	//超商門市編號
-	StoreCode string
+	StoreCode string `db:"store_code"`
 	//超商門市名稱
-	StoreName string
+	StoreName string `db:"store_name"`
 	//超商門市地址
-	StoreAddr string
+	StoreAddr string `db:"store_addr"`
 	//取件交易方式
-	TradeType string
+	TradeType string `db:"trade_type"`
 	//取貨人
-	CVSCOMName string
+	CVSCOMName string `db:"cvs_com_name"`
 	//取貨人手機號碼
-	CVSCOMPhone string
+	CVSCOMPhone string `db:"cvs_com_phone"`
 	//物流寄件單號
-	LgsNo string
+	LgsNo string `db:"lgs_no"`
 	//物流型態
-	LgsType string
+	LgsType string `db:"lgs_type"`
 	//**跨境支付回傳參數(包含簡單付電子錢包、簡單付微信支付、簡單付支付寶)**
 	//跨境通路類型
-	ChannelID string
+	ChannelID string `db:"channel_id"`
 	//跨境通路 交易序號
-	ChannelNo string
+	ChannelNo string `db:"channel_no"`
 	//**玉山 Wallet 回傳參數** **台灣 Pay 回傳參數**
 	//實際付款金額
-	PayAmt string
+	PayAmt string `db:"pay_amt"`
 	//紅利折抵金額
-	RedDisAmt string
+	RedDisAmt string `db:"red_dis_amt"`
 }
 
 func NewMPGGatewayResult() *MPGGatewayResult {
